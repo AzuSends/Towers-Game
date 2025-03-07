6b6aad8f17fd32a500302857126daa3658e0d266
AzuDreams <147209069+AzuSends@users.noreply.github.com>
2025-03-07 14:22:06 -0800

    Very Basic Combat Logic

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,7 +2,7 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene")
     }
-    
+
     create() {
         this.board = [];
         this.terrain = [];
@@ -13,23 +13,23 @@ class Play extends Phaser.Scene {
         this.objectBoard = this.add.group();
 
         this.graphics.fillStyle(0x40c72c, 1); //Color const
-        this.graphics.fillRect(0, 0, 57,57); //Needs cleanup, should be easy to determine where squares are so that mouse controls can be easily implemented
-        this.graphics.generateTexture("squareTextureGreen", 57,57)
+        this.graphics.fillRect(0, 0, 57, 57); //Needs cleanup, should be easy to determine where squares are so that mouse controls can be easily implemented
+        this.graphics.generateTexture("squareTextureGreen", 57, 57)
 
         this.graphics.fillStyle(0x71acf0, 1); //Color const
-        this.graphics.fillRect(0, 0, 57,57); //^
-        this.graphics.generateTexture("squareTexture", 57,57)
+        this.graphics.fillRect(0, 0, 57, 57); //^
+        this.graphics.generateTexture("squareTexture", 57, 57)
 
         this.graphics.fillStyle(0x000000, 1); //Should be a const?
         this.graphics.fillRect(0, 0, 57, 26.5); //310 + 60i, 55+60j
-        this.graphics.generateTexture("blackUnit", 57,26.5)
+        this.graphics.generateTexture("blackUnit", 57, 26.5)
 
         this.graphics.fillStyle(0xffffff, 1); //Should be a const?
         this.graphics.fillRect(0, 0, 57, 26.5); //310 + 60i, 25+60j
-        this.graphics.generateTexture("whiteUnit", 57,26.5)
+        this.graphics.generateTexture("whiteUnit", 57, 26.5)
 
         this.graphics.destroy();
-        
+
 
         for (let i = 0; i < 11; i++) {
             this.board[i] = [];
@@ -37,33 +37,34 @@ class Play extends Phaser.Scene {
             this.objectBoard[i] = [];
             for (let j = 0; j < 9; j++) {
                 this.board[i][j] = 0;
-                if (([1, 2].includes(i) && [1, 2].includes(j))|| [8, 9].includes(i) && [6, 7].includes(j)){
+                if (([1, 2].includes(i) && [1, 2].includes(j)) || [8, 9].includes(i) && [6, 7].includes(j)) {
                     this.terrain[i][j] = 1
-                    
-                    var square = new BoardSquare(this, boardOffestX + 60*i, boardOffsetY + 60*j, "squareTextureGreen", 0)
+
+                    var square = new BoardSquare(this, boardOffestX + 60 * i, boardOffsetY + 60 * j, "squareTextureGreen", 0)
                     this.objectBoard[i].push(square)
-                } else{
+                } else {
                     this.terrain[i][j] = 0
-                    
-                    var square = new BoardSquare(this, boardOffestX + 60*i, boardOffsetY + 60*j, "squareTexture", 0)
+
+                    var square = new BoardSquare(this, boardOffestX + 60 * i, boardOffsetY + 60 * j, "squareTexture", 0)
                     this.objectBoard[i].push(square)
 
                 }
             }
         }
-        this.board[6][5] = new Unit(this, boardOffestX + 60*6, boardOffsetY - unitOffsetY + 60*5, "whiteUnit", 0, 0, 6, 5)
-        this.board[5][5] = new Unit(this, boardOffestX + 60*5, boardOffsetY - unitOffsetY + 60*5, "whiteUnit", 0, 0, 5, 5)
-        this.board[5][4] = new Unit(this, boardOffestX + 60*5, boardOffsetY + unitOffsetY + 60*4, "blackUnit", 0, 1, 5, 4)
-        this.board[6][4] = new Unit(this, boardOffestX + 60*6, boardOffsetY + unitOffsetY + 60*4, "blackUnit", 0, 1, 6, 4)
+        this.board[6][5] = new Unit(this, boardOffestX + 60 * 6, boardOffsetY - unitOffsetY + 60 * 5, "whiteUnit", 0, 0, 6, 5, "calv1")
+        this.board[5][5] = new Unit(this, boardOffestX + 60 * 5, boardOffsetY - unitOffsetY + 60 * 5, "whiteUnit", 0, 0, 5, 5, "calv2")
+        this.board[5][4] = new Unit(this, boardOffestX + 60 * 5, boardOffsetY + unitOffsetY + 60 * 4, "blackUnit", 0, 1, 5, 4, "calv3")
+        this.board[6][4] = new Unit(this, boardOffestX + 60 * 6, boardOffsetY + unitOffsetY + 60 * 4, "blackUnit", 0, 1, 6, 4, "calv4")
+        this.board[2][2] = new Unit(this, boardOffestX + 60 * 2, boardOffsetY + unitOffsetY + 60 * 2, "blackUnit", 0, 1, 2, 2, "archer")
 
         this.determineBonuses();
 
-        
 
 
 
 
-        
+
+
 
 
         // Draw a hand of card for each player, player 1 will begin so I will show their hand first
@@ -102,47 +103,99 @@ class Play extends Phaser.Scene {
 
     }
 
-    resolveCombat(){
+    resolveCombat() {
 
     }
 
-    determineBonuses(){
-        for (let i = 0; i < 11; i++){
-            for(let j = 0; j < 9; j++){
-                if (this.board[i][j] != 0){
-                    this.support(i,j)
+    determineBonuses() {
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (this.board[i][j] != 0) {
+                    this.support(i, j)
+                    this.flank(i, j)
+                    this.attack(i, j)
+                    this.checkHighGround(i, j)
                 }
             }
         }
     }
 
-    support(i,j){
-        if (this.board[i-1][j] != 0 || this.board[i-1][j] != undefined){
-            if (this.board[i-1][j].player == this.board[i][j].player){
-                this.board[i][j].giveSupport(this.board[i-1][j])
+    support(i, j) {
+        if (this.board[i - 1][j] != 0 && this.board[i - 1][j] != undefined) {
+            if (this.board[i - 1][j].player == this.board[i][j].player) {
+                this.board[i][j].giveSupport(this.board[i - 1][j])
             }
         }
-        if (this.board[i][j-1] != 0 || this.board[i][j-1] != undefined){
-            if (this.board[i][j-1].player == this.board[i][j].player){
-                this.board[i][j].giveSupport(this.board[i][j-1])
+        if (this.board[i][j - 1] != 0 && this.board[i][j - 1] != undefined) {
+            if (this.board[i][j - 1].player == this.board[i][j].player) {
+                this.board[i][j].giveSupport(this.board[i][j - 1])
             }
         }
-        if (this.board[i+1][j] != 0 || this.board[i+1][j] != undefined){
-            if (this.board[i+1][j].player == this.board[i][j].player){
-                this.board[i][j].giveSupport(this.board[i+1][j])
+        if (this.board[i + 1][j] != 0 && this.board[i + 1][j] != undefined) {
+            if (this.board[i + 1][j].player == this.board[i][j].player) {
+                this.board[i][j].giveSupport(this.board[i + 1][j])
             }
         }
-        if (this.board[i][j+1] != 0 || this.board[i][j+1] != undefined){
-            if (this.board[i][j+1].player == this.board[i][j].player){
-                this.board[i][j].giveSupport(this.board[i][j+1])
+        if (this.board[i][j + 1] != 0 && this.board[i][j + 1] != undefined) {
+            if (this.board[i][j + 1].player == this.board[i][j].player) {
+                this.board[i][j].giveSupport(this.board[i][j + 1])
             }
         }
     }
 
-    flank(){
-        
+    flank(i, j) {
+        if (this.board[i - 1][j - 1] != 0 && this.board[i - 1][j - 1] != undefined) {
+            if (this.board[i - 1][j - 1].player != this.board[i][j].player) {
+                this.board[i][j].applyFlank(this.board[i - 1][j - 1])
+            }
+        }
+        if (this.board[i - 1][j + 1] != 0 && this.board[i - 1][j + 1] != undefined) {
+            if (this.board[i - 1][j + 1].player != this.board[i][j].player) {
+                this.board[i][j].applyFlank(this.board[i - 1][j + 1])
+            }
+        }
+        if (this.board[i + 1][j - 1] != 0 && this.board[i + 1][j - 1] != undefined) {
+            if (this.board[i + 1][j - 1].player != this.board[i][j].player) {
+                this.board[i][j].applyFlank(this.board[i + 1][j - 1])
+            }
+        }
+        if (this.board[i + 1][j + 1] != 0 && this.board[i + 1][j + 1] != undefined) {
+            if (this.board[i + 1][j + 1].player != this.board[i][j].player) {
+                this.board[i][j].applyFlank(this.board[i + 1][j + 1])
+            }
+        }
+
     }
 
-    
+    attack(i, j) {
+        if (this.board[i - 1][j] != 0 && this.board[i - 1][j] != undefined) {
+            if (this.board[i - 1][j].player != this.board[i][j].player) {
+                this.board[i][j].fight(this.board[i - 1][j])
+            }
+        }
+        if (this.board[i][j - 1] != 0 && this.board[i][j - 1] != undefined) {
+            if (this.board[i][j - 1].player != this.board[i][j].player) {
+                this.board[i][j].fight(this.board[i][j - 1])
+            }
+        }
+        if (this.board[i + 1][j] != 0 && this.board[i + 1][j] != undefined) {
+            if (this.board[i + 1][j].player != this.board[i][j].player) {
+                this.board[i][j].fight(this.board[i + 1][j])
+            }
+        }
+        if (this.board[i][j + 1] != 0 && this.board[i][j + 1] != undefined) {
+            if (this.board[i][j + 1].player != this.board[i][j].player) {
+                this.board[i][j].fight(this.board[i][j + 1])
+            }
+        }
+    }
+
+    checkHighGround(i, j) {
+        if (this.terrain[i][j] == 1) {
+            this.board[i][j].highGround();
+        }
+    }
+
+
 
 }
--- src/scenes/Unit.js
+++ src/scenes/Unit.js
@@ -1,35 +1,53 @@
 class Unit extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, player, boardX, boardY) {
-      super(scene, x, y, texture, frame)
-      this.player = player;
-      this.boardX = boardX;
-      this.boardY = boardY;
-      this.attack = 0;
-      this.attackBonus = 0;
-      this.defense = 0;
-      this.defenseBonus = 0;
-      this.range = 0;
-      this.moveSpeed = 0;
-      this.position = 0;
-      this.flankBonus = 0;
-      this.supportBonus = 0;
-      
+  constructor(scene, x, y, texture, frame, player, boardX, boardY, unitName) {
+    super(scene, x, y, texture, frame)
+    this.player = player;
+    this.boardX = boardX;
+    this.boardY = boardY;
+    this.attack = 0;
+    this.attackBonus = 0;
+    this.defense = 0;
+    this.defensePenalty = 0;
+    this.range = 0;
+    this.moveSpeed = 0;
+    this.position = 0;
+    this.flankBonus = 0;
+    this.supportBonus = 0;
+    this.highGroundBonus = 0;
+    this.unitName = unitName
 
 
-  
-      // add object to existing scene
-      scene.add.existing(this)
-    }
 
-    update(){
-        //Nothing needed here as this will mostly be used as a class to manage combat and troop position
-    }
-    ownedBy(){
-      return this.player;
-    }
 
-    giveSupport(ally){
-      ally.attackBonus += this.supportBonus
-    }
-    
+    // add object to existing scene
+    scene.add.existing(this)
   }
+
+  update() {
+    //Nothing needed here as this will mostly be used as a class to manage combat and troop position
+  }
+  ownedBy() {
+    return this.player;
+  }
+
+  giveSupport(ally) {
+    ally.attackBonus += this.supportBonus
+    console.log(this.unitName + " applied support to " + ally.unitName)
+  }
+
+  applyFlank(enemy) {
+    enemy.defensePenalty -= this.flankBonus
+    console.log(this.unitName + " is flanking " + enemy.unitName)
+  }
+
+  fight(enemy) {
+    if ((this.attack + this.attackBonus) >= (enemy.defense - enemy.defensePenalty)) {
+      console.log(this.unitName + " destroyed " + enemy.unitName)
+    }
+  }
+
+  highGround() {
+    console.log(this.unitName + " has the high ground ")
+  }
+
+}
