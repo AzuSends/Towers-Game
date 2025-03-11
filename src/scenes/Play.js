34a240d796fa5848909af163aec96add00b6de6f
Benjamin Silver <besilver@ucsc.edu>
2025-03-10 19:07:38 -0700

    cleaned up unnecessary comments

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -95,32 +95,6 @@ class Play extends Phaser.Scene {
             console.log("Turn Passed to Opponent")
         }
 
-        //On click draw the possible moves and 
-
-
-        //Current player will be set and used as a variable to avoid nesting conditionals
-
-        //Game will wait for mouse actions
-
-        //Players may select a unit and move it, deploy a unit from their hand, or retreat a unit into their hand
-
-        //Players will be able to make a set number of moves no matter the number of troops they have commited 
-
-        //Check for move counter will pass the turn to the other player
-
-        //After both players have gone units on the board will fight 
-
-        //Troop positions will be stored as row and column allowing for distances to be calculated simply
-
-        //Most calculations will be done with brute force since I don't currently plan on adding a computer player that would need to look ahead
-
-        //Units will first update combat values based on supporting units, flanking, or high ground
-
-        //Units will then compare combat values and fight
-
-        //Turn will be passed back to player 1 
-
-        //Loop will continue until a player has no more units on the board
 
 
     }
